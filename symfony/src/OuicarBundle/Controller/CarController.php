c5f7275a5dc82ec66b5d7546c7fcf4f46ff4c27d
bersiroth <caronbernard1@gmail.com>
2018-01-24 02:35:58 +0100

    wip : add routing

--- symfony/src/OuicarBundle/Controller/CarController.php
+++ symfony/src/OuicarBundle/Controller/CarController.php
@@ -2,12 +2,54 @@
 
 namespace OuicarBundle\Controller;
 
+use phpDocumentor\Reflection\Types\Boolean;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\Request;
+use OuicarBundle\Entity\Car;
+use OuicarBundle\Form\CarType;
 
+/**
+ * @Rest\RouteResource("Car", pluralize=false)
+ */
 class CarController extends Controller
 {
-    public function createCarAction()
+    /**
+     * @Rest\View()
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function postAction(Request $request)
     {
+        $car = new Car();
+        $form = $this->createForm(CarType::class, $car);
+        $form->submit($request->request->all());
 
+        if ($form->isValid() ) {
+            $em = $this->get('doctrine.orm.entity_manager');
+            $em->persist($car);
+            $em->flush();
+            return $car;
+        } else {
+            return $form;
+        }
+    }
+
+    /**
+     * @param Car|null $car
+     * @param Request|null $request
+     * @return Form
+     */
+    protected function getForm(Car $car = null, Request $request = null): Form
+    {
+        $form = $this->createForm(CarType::class, $car ?: new Car());
+
+        if ($request) {
+            $form->handleRequest($request);
+        }
+
+        return $form;
     }
 }
--- symfony/src/OuicarBundle/Form/CarType.php
+++ symfony/src/OuicarBundle/Form/CarType.php
@@ -26,7 +26,8 @@ class CarType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'OuicarBundle\Entity\Car'
+            'data_class' => 'OuicarBundle\Entity\Car',
+            'csrf_protection' => false
         ));
     }
 
